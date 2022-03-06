d4fbc8e791dd02e06e0e776d680bec25f71ef569
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-03-07 00:42:55 +0100

    mod 20 done

---
+++ mod_20-time-to-practice-starting-code/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Quotes from "./pages/Quotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <Quotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+    </Layout>
+  );
+}
+
+export default App;
